b103565e25687c429ed64164198dd04150571534
Gabriel Costa <1496860+GabrielFCosta@users.noreply.github.com>
2020-07-08 23:08:45 -0300

    Delete names.R

--- names.R
+++
@@ -1,5 +0,0 @@
-## reading the column names vector (features.txt)
-cols <- read.table("./dataset/features.txt")
-cols <- cols$V2
-## appending subject and activity to column labels vector
-cols <- append(cols,c("activity","subject"))
